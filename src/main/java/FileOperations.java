0a95ccd034727e0a7f5adf81f0c1c336b2138509
Namit Dadlani <namitdadlani@gmail.com>
2022-05-18 17:58:50 -0300

    Relocated test doc, stored test details in static vars

--- src/main/java/FileOperations.java
+++ src/main/java/FileOperations.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class FileOperations {
 
-    //TODO: Add comments for methods.
+    static public String TEST_PATH_DOC = ".\\docs\\";
+    static public String TEST_FILE1 = "testFile.txt";
 
+    /*
+    * Method: fetchFileFromPath
+    * Description: Fetches any file from a given path.
+    */
     public static File fetchFileFromPath(String filePath, String fileName) {
         File file = new File(filePath+fileName);
         return file;
     }
 
+    /*
+     * Method: readFileLineByLine
+     * Description: Accesses file contents line-by-line.
+     */
     public static void readFileLineByLine(String filePath, String fileName) {
         File fetchedfile = fetchFileFromPath(filePath, fileName);
         try {
@@ -25,7 +34,7 @@ public class FileOperations {
     }
 
     public static void main(String[] args) {
-        readFileLineByLine("", "testFile.txt");
+        readFileLineByLine(TEST_PATH_DOC, TEST_FILE1);
     }
 
 }
